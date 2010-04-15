22c557deda3196948a53b89a0bc9f1b500397471
Bryan Shelton <bryan@sheltonopensolutions.com>
2010-04-15 16:47:10 -0600

    Added binary change

--- lib/rmybackup.rb
+++ lib/rmybackup.rb
@@ -4,11 +4,29 @@ require 'yaml'
 class RMyBackup
   def initialize(config_file)
     @config_file = config_file
-    parse_config
-    @config['databases'].each {|db| puts "DB - #{db}\n" }
+    #if the config has been parsed correctly, run the backups
+    run_backups if parse_config
+  end
+  
+  private
+  #Run the backups, we should have proper validation at this point
+  def run_backups
+
+    #Grab some config variables
+    mysql_dump = @config['mysqldump_command']
+    backup_dir = @config['backup_dir']
+
+    #Cycle through databases
+    puts "We're going to backup\n--------------------"
+    @config['databases'].each do |db|
+      puts "#{db}\n"
+    end
   end
   
   def parse_config
-   @config = YAML::load(File.open(@config_file))
+    @config = YAML::load(File.open(@config_file))
+    return true
+  rescue
+    return false
   end
 end
